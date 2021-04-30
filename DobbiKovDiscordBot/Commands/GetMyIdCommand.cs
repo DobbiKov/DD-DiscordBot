03162bf7ddc2082bff73ac2d1d9695768e74ead6
DobbiKov <dobbikov@gmail.com>
2021-04-30 14:11:21 +0300

    initial commit

---
+++ DobbiKovDiscordBot/Commands/GetMyIdCommand.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobbiKovDiscordBot.Commands
+{
+    public class GetMyIdCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("getmyid")]
+        public async Task GetMyIdAsync()
+        {
+            var user = Context.User;
+            await ReplyAsync($"{user.Username}, ваш ID: {user.Id}");
+        }
+    }
+}
---
+++ DobbiKovDiscordBot/Program.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DobbiKovDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = "ODM3NjQzODY0MzEwMjg0Mjk4.YIviow.0PPNQgYLgWVzBK3roohrrx5Cc2s";
+
+            client.Log += clientLog;
+
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if(!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition))
+                    await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+
+        private Task clientLog(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+    }
+}
